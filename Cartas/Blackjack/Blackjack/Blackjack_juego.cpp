e74229e8e95519dcbae88aae22af6a413f44b1b0
Algoca05 <153614337+Algoca05@users.noreply.github.com>
2024-02-08 18:30:15 +0100

    Update Blackjack_juego.cpp

--- Cartas/Blackjack/Blackjack/Blackjack_juego.cpp
+++ Cartas/Blackjack/Blackjack/Blackjack_juego.cpp
@@ -50,8 +50,9 @@ void numCard(int& repCards, string& suit, int& num, char play, int& iaCard) {
 			cout << "You AS of " << suit << " is a 1 or an 11\n";
 			cin >> num;
 			repCards = num;
-			cout << repCards << " of " << suit << "\n";
+			
 		} while (num != 1 && num != 11);
+		cout << repCards << " of " << suit << "\n";
 	}
 	if (repCards == 0 && play == 'n')
 	{
